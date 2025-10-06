9cc100d399e575cb9df9680f0580f8ca155f419a
Buster210-IITM <22f2001388@ds.study.iitm.ac.in>
2025-10-06 22:07:53 +0530

    add CORS

--- api/main.py
+++ api/main.py
@@ -1,9 +1,19 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title="LLM Code Deployment API", version="0.1.0")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    expose_headers=["*"],
+)
 
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+def health():
+    return {"message": "api is working"}
